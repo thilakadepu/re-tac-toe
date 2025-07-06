6cdac71c048213618c8c9f932ae926680f0b0248
- <145832238+thilakadepu@users.noreply.github.com>
2025-07-06 11:17:34 +0530

    implement the cors config and api controller for basic

---
+++ Back-End/re-tac-toe/src/main/java/com/game/re_tac_toe/controller/ApiController.java
@@ -0,0 +1,16 @@
+package com.game.re_tac_toe.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+   @GetMapping("/hello")
+   public String sayHello() {
+       return "Hello from backend!";
+   }
+
+}
---
+++ Back-End/re-tac-toe/src/test/java/com/game/re_tac_toe/ReTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.re_tac_toe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
