e4a3b91c635c4973fdf11529177daad11ca5d7fd
- <145832238+thilakadepu@users.noreply.github.com>
2025-07-06 11:16:59 +0530

    Initial commit with essentials

---
+++ Back-End/re-tac-toe/src/main/java/com/game/re_tac_toe/ReTacToeApplication.java
@@ -0,0 +1,13 @@
+package com.game.re_tac_toe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReTacToeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReTacToeApplication.class, args);
+	}
+
+}
