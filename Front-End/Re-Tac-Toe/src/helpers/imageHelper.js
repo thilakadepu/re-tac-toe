eb7d6c746bac68127b970798c18faf425e75159c
- <145832238+thilakadepu@users.noreply.github.com>
2025-07-22 21:21:39 +0530

    Added new images and favicon.ico

--- Front-End/Re-Tac-Toe/src/helpers/imageHelper.js
+++ Front-End/Re-Tac-Toe/src/helpers/imageHelper.js
@@ -1,14 +1,32 @@
-import duskull from "../assets/avatars/Duskull.png";
-import marill from "../assets/avatars/Marill.png";
-import meowth from "../assets/avatars/Meowth.png";
-import pikachu from "../assets/avatars/Pikachu.png";
-import voltorb from "../assets/avatars/Voltorb.png";
+import bellsprout from "../assets/avatars/Bellsprout.png"
+import diglett from "../assets/avatars/Diglett.png"
+import duskull from "../assets/avatars/Duskull.png"
+import gastly from "../assets/avatars/Gastly.png"
+import gloom from "../assets/avatars/Gloom.png"
+import jigglypuff from "../assets/avatars/Jigglypuff.png"
+import marill from "../assets/avatars/Marill.png"
+import meowth from "../assets/avatars/Meowth.png"
+import munchlax from "../assets/avatars/Munchlax.png"
+import oddish from "../assets/avatars/Oddish.png"
+import pikachu from "../assets/avatars/Pikachu.png"
+import psyduck from "../assets/avatars/Psyduck.png"
+import staryu from "../assets/avatars/Staryu.png"
+import voltorb from "../assets/avatars/Voltorb.png"
 
 const imageMap = {
+  "Bellsprout.png": bellsprout,
+  "Diglett.png": diglett,
   "Duskull.png": duskull,
+  "Gastly.png": gastly,
+  "Gloom.png": gloom,
+  "Jigglypuff.png": jigglypuff,
   "Marill.png": marill,
   "Meowth.png": meowth,
+  "Munchlax.png": munchlax,
+  "Oddish.png": oddish,
   "Pikachu.png": pikachu,
+  "Psyduck.png": psyduck,
+  "Staryu.png": staryu,
   "Voltorb.png": voltorb,
 }
 
